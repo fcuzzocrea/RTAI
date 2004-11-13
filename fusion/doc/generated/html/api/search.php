91163978066de0388028ce73937d99ea1d1ccdf0
Gilles Chanteperdrix <gilles.chanteperdrix@xenomai.org>
2004-11-13 21:52:44 +0000

    Generated doc update

--- fusion/doc/generated/html/api/search.php
+++ fusion/doc/generated/html/api/search.php
@@ -350,7 +350,7 @@ main();
 
 
 ?>
-<hr size="1"><address style="align: right;"><small>Generated on Tue Oct 26 00:09:57 2004 for RTAI API by&nbsp;
+<hr size="1"><address style="align: right;"><small>Generated on Sat Nov 13 22:39:56 2004 for RTAI API by&nbsp;
 <a href="http://www.doxygen.org/index.html">
 <img src="doxygen.png" alt="doxygen" align="middle" border="0"></a> 1.3.9.1 </small></address>
 </body>

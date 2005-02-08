c1c6bc04e51549f0a66066ec613b22a56682d7c1
Philippe Gerum <rpm@xenomai.org>
2005-02-08 23:10:19 +0000

    Drain __fusion_user_init

--- fusion/sim/gcic/gcic.cc
+++ fusion/sim/gcic/gcic.cc
@@ -902,7 +902,7 @@ int main (int argc, char **argv)
 
     if (FlagLinking)
 	{
-	CCCmdString += " -u main -u __fusion_skin_init";
+	CCCmdString += " -u main -u __fusion_skin_init -u __fusion_user_init";
 
 	CString libDir(ExecPrefix);
 	libDir += "lib";

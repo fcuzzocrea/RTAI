8beabbcf2427e779b95a6131757a700e0f8df326
Philippe Gerum <rpm@xenomai.org>
2005-02-21 09:52:51 +0000

    Engage IRQ shield upon switch to secondary mode

--- fusion/nucleus/shadow.c
+++ fusion/nucleus/shadow.c
@@ -506,6 +506,8 @@ void xnshadow_relax (void)
 
     xnltt_log_event(rtai_ev_secondarysw,thread->name);
 
+    engage_irq_shield();
+
     if (current->state & TASK_UNINTERRUPTIBLE)
 	/* Just to avoid wrecking Linux's accounting of non-
 	   interruptible tasks, move back kicked tasks to

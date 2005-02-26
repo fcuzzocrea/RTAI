5b6050fcc5893448076ee708175a0a5d7182575e
Gilles Chanteperdrix <gilles.chanteperdrix@xenomai.org>
2005-02-26 22:25:43 +0000

    blank space change & cosmetic change

--- fusion/testsuite/klatency/latency.c
+++ fusion/testsuite/klatency/latency.c
@@ -8,44 +8,44 @@
 int main (int argc, char **argv)
 
 {
+    const char *const communication_channel = "/dev/rtp0";
     struct rtai_latency_stat s;
     ssize_t sz;
+    int n = 0;
     int fd;
-	const char *communication_channel = "/dev/rtp0";
-	int n = 0;
     
-	setlinebuf(stdout);
+    setlinebuf(stdout);
     
     fd = open(communication_channel, O_RDWR);
     
     if (fd < 0)
-	{
-	perror(communication_channel);
-	exit(1);
-	}
+        {
+        fprintf(stderr, "open(%s): %m\n", communication_channel);
+        exit(1);
+        }
 
     for (;;)
-	{
-	sz = read(fd,&s,sizeof(s));
+        {
+        sz = read(fd,&s,sizeof(s));
 
-	if (!sz)
-	    break;
-	
-	if (sz != sizeof(s))
-	    {
-	    perror("read");
-	    exit(1);
-	    }
+        if (!sz)
+            break;
+        
+        if (sz != sizeof(s))
+            {
+            perror("read");
+            exit(1);
+            }
 
-	if ((n++ % 21)==0)
-	    printf("RTH|%12s|%12s|%12s|%12s\n", "jit min","jit avg","jit max","overrun");
+        if ((n++ % 21)==0)
+            printf("RTH|%12s|%12s|%12s|%12s\n", "jit min","jit avg","jit max","overrun");
 
-	printf("RTD|%12d|%12d|%12d|%12d\n",
-	       s.minjitter,
-	       s.avgjitter,
-	       s.maxjitter,
-	       s.overrun);
-	}
+        printf("RTD|%12d|%12d|%12d|%12d\n",
+               s.minjitter,
+               s.avgjitter,
+               s.maxjitter,
+               s.overrun);
+        }
 
     return 0;
 }

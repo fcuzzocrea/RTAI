e9dfdf7d6a6b9379d89229b7a1a692626b3543f2
Philippe Gerum <rpm@xenomai.org>
2005-03-07 16:33:58 +0000

    Add placeholder for __builtin_expect()

--- fusion/include/nucleus/asm-i386/atomic.h
+++ fusion/include/nucleus/asm-i386/atomic.h
@@ -34,6 +34,9 @@
 #else /* !__KERNEL__ */
 
 #ifndef likely
+#if __GNUC__ == 2 && __GNUC_MINOR__ < 96
+#define __builtin_expect(x, expected_value) (x)
+#endif
 #define likely(x)	__builtin_expect(!!(x), 1)
 #define unlikely(x)	__builtin_expect(!!(x), 0)
 #endif /* !likely */

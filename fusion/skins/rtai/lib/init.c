3d8539574feaafc228c1ae4799a61c2f29018492
Philippe Gerum <rpm@xenomai.org>
2005-06-04 20:58:24 +0000

    Fix header inclusion

--- fusion/skins/rtai/lib/init.c
+++ fusion/skins/rtai/lib/init.c
@@ -16,12 +16,9 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
  */
 
-#include <sys/types.h>
-#include <sys/mman.h>
-#include <memory.h>
 #include <malloc.h>
-#include <unistd.h>
-#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <rtai/syscall.h>
 #include <rtai/task.h>
 

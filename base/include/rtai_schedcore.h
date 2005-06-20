c0a8c38bac030ce0c3c4dc359ba657e3e57d898e
Paolo Mantegazza <paolo.mantegazza@polimi.it>
2005-06-20 12:48:01 +0000

    fixed a bad use of posticrements

--- base/include/rtai_schedcore.h
+++ base/include/rtai_schedcore.h
@@ -192,7 +192,8 @@ extern struct klist_t wake_up_srq;
 
 #define pend_wake_up_srq(lnxtsk, cpuid) \
 do { \
-	wake_up_srq.task[wake_up_srq.in++ & (MAX_WAKEUP_SRQ - 1)] = lnxtsk; \
+	wake_up_srq.task[wake_up_srq.in & (MAX_WAKEUP_SRQ - 1)] = lnxtsk; \
+	wake_up_srq.in++; \
 	adeos_pend_uncond(wake_up_srq.srq, cpuid); \
 } while (0)
 

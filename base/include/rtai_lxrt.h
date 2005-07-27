b421474216f81e18a9f1e45887de52e396676d20
Paolo Mantegazza <paolo.mantegazza@polimi.it>
2005-07-27 10:39:28 +0000

    LXRT enc/decoding restructured to make more room for something else

--- base/include/rtai_lxrt.h
+++ base/include/rtai_lxrt.h
@@ -397,12 +397,12 @@
 
 // Keep LXRT call enc/decoding together, so you are sure to act consistently.
 // This is the encoding, note " | GT_NR_SYSCALLS" to ensure not a Linux syscall, ...
-#define GT_NR_SYSCALLS  (1 << 15)
-#define ENCODE_LXRT_REQ(dynx, srq, lsize)  (((dynx) << 28) | (((srq) & 0xFFF) << 16) | GT_NR_SYSCALLS | (lsize))
+#define GT_NR_SYSCALLS  (1 << 11)
+#define ENCODE_LXRT_REQ(dynx, srq, lsize)  (((dynx) << 24) | ((srq) << 12) | GT_NR_SYSCALLS | (lsize))
 // ... and this is the decoding.
-#define SRQ(x)   (((x) >> 16) & 0xFFF)
+#define SRQ(x)   (((x) >> 12) & 0xFFF)
 #define NARG(x)  ((x) & (GT_NR_SYSCALLS - 1))
-#define INDX(x)  (((x) >> 28) & 0xF)
+#define INDX(x)  (((x) >> 24) & 0xF)
 
 #ifdef __KERNEL__
 

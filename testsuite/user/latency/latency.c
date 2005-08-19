9ded9ff38aceaba254044e318c13e8a1a5b7447e
Paolo Mantegazza <paolo.mantegazza@polimi.it>
2005-08-19 18:59:41 +0000

    show partial maxes

--- testsuite/user/latency/latency.c
+++ testsuite/user/latency/latency.c
@@ -26,7 +26,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
 #include <rtai_mbx.h>
 #include <rtai_msg.h>
 
-#define OVERALL
+//#define OVERALL
 
 #define AVRGTIME    1
 #if defined(CONFIG_UCLINUX) || defined(CONFIG_ARM)

ef08bcd95166d183adef167756810b7432ecf9de
Roberto Bucher <roberto.bucher@supsi.ch>
2005-12-28 14:00:20 +0000

    *** empty log message ***

--- rtai-lab/scilab/devices/devtmpl.h
+++ rtai-lab/scilab/devices/devtmpl.h
@@ -1,10 +1,10 @@
-void inp_xxx_init(int port,int nch,char * sName,char * sParam,double p1,
+int inp_xxx_init(int port,int nch,char * sName,char * sParam,double p1,
                   double p2, double p3, double p4, double p5);
 void inp_xxx_input(int port, double * y, double t);
 void inp_xxx_update(void);
 void inp_xxx_end(int port);
 
-void out_xxx_init(int port,int nch,char * sName,char * sParam,double p1,
+int out_xxx_init(int port,int nch,char * sName,char * sParam,double p1,
                   double p2, double p3, double p4, double p5);
 void out_xxx_output(int port, double * u,double t);
 void out_xxx_end(int port);

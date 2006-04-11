ac287c7351248b62c23e31c9cfacff3ce3e67046
Paolo Mantegazza <paolo.mantegazza@polimi.it>
2006-04-11 21:31:07 +0000

    *** empty log message ***

--- base/sched/sched.c
+++ base/sched/sched.c
@@ -613,7 +613,24 @@ do { \
 		} \
 	} \
 
-#define _SELF_SUSP_IN_IRQ() \
+#ifdef LOCKED_LINUX_IN_IRQ_HANDLER
+#define LOCK_LINUX_IN_IRQ(cpuid)
+#define UNLOCK_LINUX_IN_IRQ(cpuid)
+#define SELF_SUSP_IN_IRQ() \
+	if (rt_current->state & RT_SCHED_SELFSUSP) { \
+		rem_ready_current(rt_current); \
+		if (!rt_scheduling[cpuid].locked) { \
+			UNLOCK_LINUX(cpuid); \
+			sched_get_global_lock(cpuid); \
+			rt_schedule(); \
+			sched_release_global_lock(cpuid); \
+			LOCK_LINUX(cpuid); \
+		} \
+	}
+#else
+#define LOCK_LINUX_IN_IRQ(cpuid)    LOCK_LINUX(cpuid)    
+#define UNLOCK_LINUX_IN_IRQ(cpuid)  UNLOCK_LINUX(cpuid)
+#define SELF_SUSP_IN_IRQ() \
 	if (rt_current->state & RT_SCHED_SELFSUSP) { \
 		rem_ready_current(rt_current); \
 		if (!rt_scheduling[cpuid].locked) { \
@@ -621,21 +638,7 @@ do { \
 			rt_schedule(); \
 			sched_release_global_lock(cpuid); \
 		} \
-	} \
-
-#ifdef LOCKED_LINUX_IN_IRQ_HANDLER
-#define LOCK_LINUX_IN_IRQ(cpuid)
-#define UNLOCK_LINUX_IN_IRQ(cpuid)
-#define SELF_SUSP_IN_IRQ() \
-	do { \
-		UNLOCK_LINUX(cpuid); \
-		_SELF_SUSP_IN_IRQ(); \
-		LOCK_LINUX(cpuid); \
-	} while (0)
-#else
-#define LOCK_LINUX_IN_IRQ(cpuid)    LOCK_LINUX(cpuid)    
-#define UNLOCK_LINUX_IN_IRQ(cpuid)  UNLOCK_LINUX(cpuid)
-#define SELF_SUSP_IN_IRQ  _SELF_SUSP_IN_IRQ
+	}
 #endif
 
 #if CONFIG_RTAI_MONITOR_EXECTIME

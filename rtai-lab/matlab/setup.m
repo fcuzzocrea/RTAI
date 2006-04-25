e973b077232264555937e51225200350093e8644
Roberto Bucher <roberto.bucher@supsi.ch>
2006-04-25 09:19:37 +0000

    New version from Arno Stienen

--- rtai-lab/matlab/setup.m
+++ rtai-lab/matlab/setup.m
@@ -1,8 +1,20 @@
-function setup()
+% *******
+% setup.m
+% *******
+%
+% Don't forget to copy ptinfo.tlc to [matlabroot]\rtw\c\tlc\mw if you're
+% using Matlab 7.0 or newer.
 
 devices = [matlabroot, '/rtw/c/rtai/devices'];
 addpath(devices);
-devices = [matlabroot, '/rtw/c/rtai/lib'];
-addpath(devices);
-path2rc;
+% devices = [matlabroot, '/rtw/c/rtai/lib'];
+% addpath(devices);
+savepath;
 
+cd devices
+sfuns = dir('*.c')
+for cnt = 1:length(sfuns)
+    eval(['mex ' sfuns(cnt).name])
+end
+
+% ****************

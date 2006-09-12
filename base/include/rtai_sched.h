be3334473de7f9c2dea683ee1bb68c775e4517d7
Paolo Mantegazza <paolo.mantegazza@polimi.it>
2006-09-12 18:47:27 +0000

    just styling

--- base/include/rtai_sched.h
+++ base/include/rtai_sched.h
@@ -35,7 +35,7 @@
 #define RT_SCHED_LOWEST_PRIORITY   0x3fffFfff
 #define RT_SCHED_LINUX_PRIORITY    0x7fffFfff
 
-#define RT_RESEM_SUSPDEL  -0x7fffFfff
+#define RT_RESEM_SUSPDEL  (-0x7fffFfff)
 
 #define RT_SCHED_READY        1
 #define RT_SCHED_SUSPENDED    2

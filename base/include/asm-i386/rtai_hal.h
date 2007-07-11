0431a1a4f595c0aa5b1ec60b055cbd9ce416762e
Paolo Mantegazza <paolo.mantegazza@polimi.it>
2007-07-11 21:30:00 +0000

    copy Linux do_div locally

--- base/include/asm-i386/rtai_hal.h
+++ base/include/asm-i386/rtai_hal.h
@@ -104,7 +104,23 @@ static inline unsigned long long rtai_ulldiv (unsigned long long ull,
     return q.ull;
 }
 #else
-#include <asm/div64.h>
+
+/* do_div below taken from Linux-2.6.20 */
+
+#define do_div(n,base) ({ \
+        unsigned long __upper, __low, __high, __mod, __base; \
+        __base = (base); \
+        asm("":"=a" (__low), "=d" (__high):"A" (n)); \
+        __upper = __high; \
+        if (__high) { \
+                __upper = __high % (__base); \
+                __high = __high / (__base); \
+        } \
+        asm("divl %2":"=a" (__low), "=d" (__mod):"rm" (__base), "0" (__low), "1" (__upper)); \
+        asm("":"=A" (n):"a" (__low),"d" (__high)); \
+        __mod; \
+})
+
 static inline unsigned long long rtai_ulldiv (unsigned long long ull, unsigned long uld, unsigned long *r)
 {
 	if (r) {

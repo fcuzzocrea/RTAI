846175a331154a2b749e3099f7fcb7e189027eda
Paolo Mantegazza <paolo.mantegazza@polimi.it>
2008-02-13 17:08:01 +0000

    allow poll testing

--- base/include/rtai_sem.h
+++ base/include/rtai_sem.h
@@ -33,7 +33,7 @@
 #define SEM_ERR     (RTE_OBJINV)
 #define SEM_TIMOUT  (RTE_TIMOUT)
 
-//#define CONFIG_RTAI_RT_POLL
+#define CONFIG_RTAI_RT_POLL
 //#define CONFIG_RTAI_RT_POLL_ON_STACK
 
 struct rt_poll_s { void *what; unsigned long forwhat; };

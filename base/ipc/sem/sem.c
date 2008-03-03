53319ee2d5d11dd13ea2050aa6d1eade5c67e8fd
Paolo Mantegazza <paolo.mantegazza@polimi.it>
2008-03-03 22:45:10 +0000

    moved netrpc polling to netrpc.c

--- base/ipc/sem/sem.c
+++ base/ipc/sem/sem.c
@@ -2055,20 +2055,6 @@ RTAI_SYSCALL_MODE int _rt_poll(struct rt_poll_s *pdsa, unsigned long nr, RTIME t
 
 EXPORT_SYMBOL(_rt_poll);
 
-static RTAI_SYSCALL_MODE int rt_poll_netrpc(struct rt_poll_s *pdsa1, struct rt_poll_s *pdsa2, unsigned long pdsa_size, RTIME timeout)
-{
-	int ret;
-	if (sizeof(long) == 8 && !((unsigned long)pdsa1[0].what & 0xFFFFFFFF00000000ULL)) {
-		int i;
-		for (i = 0; i < pdsa_size/sizeof(struct rt_poll_s); i++) {
-			pdsa1[i].what = (void *)(((unsigned long)pdsa1[i].what | 0x80000000) ? ((unsigned long)pdsa1[i].what & 0x7FFFFFFF) | VMALLOC_START : (unsigned long)pdsa1[i].what | PAGE_OFFSET);
-		}
-	}
-	ret = _rt_poll(pdsa1, pdsa_size/sizeof(struct rt_poll_s), timeout, 1);
-	memcpy(pdsa2, pdsa1, pdsa_size);
-	return ret;
-}
-
 #endif
 
 /* +++++++++++++++++++++++++++ END POLLING SERVICE ++++++++++++++++++++++++++ */
@@ -2115,7 +2101,6 @@ struct rt_native_fun_entry rt_sem_entries[] = {
 	{ { 1, rt_spl_unlock },            SPL_UNLOCK },
 #ifdef CONFIG_RTAI_RT_POLL
 	{ { 1, _rt_poll }, 	           SEM_RT_POLL },
-	{ { 1, rt_poll_netrpc }, 	   SEM_RT_POLL_NETRPC },
 #endif
 	{ { 0, 0 },  		           000 }
 };

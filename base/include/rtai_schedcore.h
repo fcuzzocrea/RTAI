116d570dc859ffacb57f10d3387d45507372696f
Paolo Mantegazza <paolo.mantegazza@polimi.it>
2008-03-13 07:58:39 +0000

    check if current is ready before renqueuing

--- base/include/rtai_schedcore.h
+++ base/include/rtai_schedcore.h
@@ -398,9 +398,11 @@ static inline int renq_current(RT_TASK *rt_current, int priority)
 	int retval;
 	if ((retval = rt_current->priority != priority)) {
 		rt_current->priority = priority;
-		(rt_current->rprev)->rnext = rt_current->rnext;
-		(rt_current->rnext)->rprev = rt_current->rprev;
-		enq_ready_task(rt_current);
+		if (rt_current->state == RT_SCHED_READY) {
+			(rt_current->rprev)->rnext = rt_current->rnext;
+			(rt_current->rnext)->rprev = rt_current->rprev;
+			enq_ready_task(rt_current);
+		}
 	}
 	return retval;
 }

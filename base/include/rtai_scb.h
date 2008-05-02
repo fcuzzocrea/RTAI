64c4f984956141505b5e3d691c80a16d52760dfd
Paolo Mantegazza <paolo.mantegazza@polimi.it>
2008-05-02 12:39:20 +0000

    *** empty log message ***

--- base/include/rtai_scb.h
+++ base/include/rtai_scb.h
@@ -19,7 +19,7 @@
  *
  * @author Paolo Mantegazza
  *
- * @note Copyright &copy; 2004 Paolo Mantegazza <mantegazza@aero.polimi.it>
+ * @note Copyright &copy; 2008 Paolo Mantegazza <mantegazza@aero.polimi.it>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as

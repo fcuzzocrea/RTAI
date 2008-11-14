6f4f7a09955250de47b1adb9380e06fbde65c351
Paolo Mantegazza <paolo.mantegazza@polimi.it>
2008-11-14 21:57:05 +0000

    add m68knommu

--- base/config/autoconf/arch2host.sh
+++ base/config/autoconf/arch2host.sh
@@ -16,6 +16,9 @@ ppc|powerpc)
 arm)
     echo arm-unknown-linux-gnu
     ;;
+m68knommu)
+    echo m68knommu-unknown-linux-gnu
+    ;;
 "")
     # Shorthand not specified: return default value.
     echo $2

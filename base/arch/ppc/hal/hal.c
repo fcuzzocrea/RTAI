e1bf4712535e249b5934712c6cf317600d0ec9ee
Paolo Mantegazza <paolo.mantegazza@polimi.it>
2009-04-18 23:49:58 +0000

    include linux init.h

--- base/arch/ppc/hal/hal.c
+++ base/arch/ppc/hal/hal.c
@@ -35,6 +35,7 @@
 #include <linux/slab.h>
 #include <linux/errno.h>
 #include <linux/module.h>
+#include <linux/init.h>
 #include <linux/interrupt.h>
 #include <linux/irq.h>
 #include <linux/console.h>

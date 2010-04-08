af2a226af50553582b0bd2c7517f91cdace7ec70
Paolo Mantegazza <paolo.mantegazza@polimi.it>
2010-04-08 19:18:59 +0000

    added missing local copi of insns

--- addons/comedi/rtai_comedi.h
+++ addons/comedi/rtai_comedi.h
@@ -883,9 +883,10 @@ static inline int _RT_comedi_do_insnlist(unsigned long node, int port, void *dev
 		} {
 		struct rpced_insns { struct insns_ofstlens ofstlens; comedi_insn insns[n_insns]; unsigned int data_ofsts[n_insns]; lsampl_t data[maxdata]; } linsns;
 		struct { void *dev; long n_insns; void *linsns_in; void *linsns_out; unsigned long linsns_len; } arg = { dev, n_insns, &linsns, &linsns, sizeof(linsns) };
-		struct { unsigned long fun; long type; void *args; long argsize; long space; unsigned long partypes; } args = { PACKPORT(port, FUN_COMEDI_LXRT_INDX, _RT_KCOMEDI_DO_INSN_LIST, 0), UR1(3, 5) | UW1(4, 5), &arg, COMEDI_LXRT_SIZARG, 0, PARTYPES4(VADR, UINT, UINT, UINT) };
+		struct { unsigned long fun; long type; void *args; long argsize; long space; unsigned long partypes; } args = { PACKPORT(port, FUN_COMEDI_LXRT_INDX, _RT_KCOMEDI_DO_INSN_LIST, 0), UR1(3, 5) | UW1(4, 5), &arg, COMEDI_LXRT_SIZARG, 0, PARTYPES5(VADR, UINT, UINT, UINT, UINT) };
 		int retval, offset;
 		for (offset = i = 0; i < n_insns; i++) { 
+			linsns.insns[i] = insns[i];
 			memcpy(&linsns.data[offset], insns[i].data, insns[i].n*sizeof(lsampl_t));
 			linsns.data_ofsts[i] = offset*sizeof(lsampl_t);
 			offset += insns[i].n;

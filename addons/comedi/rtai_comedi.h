08a39f7095f74ca042f0821f43cd2ff70eec38b1
Paolo Mantegazza <paolo.mantegazza@polimi.it>
2010-04-16 13:14:09 +0000

    grant more appropriate returns for insn and insnlist executions

--- addons/comedi/rtai_comedi.h
+++ addons/comedi/rtai_comedi.h
@@ -402,9 +402,8 @@ RTAI_PROTO(int, comedi_do_insn, (void *dev, comedi_insn *insn))
 		int retval;
 		memcpy(ldata, linsn.data, sizeof(ldata));
 		linsn.data = ldata;
-		if ((retval = rtai_lxrt(FUN_COMEDI_LXRT_INDX, COMEDI_LXRT_SIZARG, _KCOMEDI_DO_INSN, &arg).i[LOW]) >= 0) {
-			memcpy(insn[0].data, ldata, sizeof(ldata));
-		}
+		retval = rtai_lxrt(FUN_COMEDI_LXRT_INDX, COMEDI_LXRT_SIZARG, _KCOMEDI_DO_INSN, &arg).i[LOW];
+		memcpy(insn[0].data, ldata, sizeof(ldata));
         	return retval;
 	}
 	return -1;
@@ -430,10 +429,9 @@ RTAI_PROTO(int, rt_comedi_do_insnlist, (void *dev, comedi_insnlist *ilist))
 			insns[i] = lilist.insns[i];
 		}
 		lilist.insns = insns;
-		if (!(retval = rtai_lxrt(FUN_COMEDI_LXRT_INDX, COMEDI_LXRT_SIZARG, _KCOMEDI_DO_INSN_LIST, &arg).i[LOW])) {
-			for (i = 0; i < retval; i++) { 
-				memcpy(ilist->insns[i].data, ldata[i], insns[i].n*sizeof(lsampl_t));
-			} 
+		retval = rtai_lxrt(FUN_COMEDI_LXRT_INDX, COMEDI_LXRT_SIZARG, _KCOMEDI_DO_INSN_LIST, &arg).i[LOW];
+		for (i = 0; i < retval; i++) { 
+			memcpy(ilist->insns[i].data, ldata[i], insns[i].n*sizeof(lsampl_t));
 		} }
         	return retval;
 	}

c9bcfcf2fd1bbe283c1fb93af942bb07be2ea0e8
Paolo Mantegazza <paolo.mantegazza@polimi.it>
2010-10-12 10:50:23 +0000

    used elseif in place of elif

--- base/include/asm-i386/rtai_fpu.h
+++ base/include/asm-i386/rtai_fpu.h
@@ -35,7 +35,7 @@
 #if LINUX_VERSION_CODE <= KERNEL_VERSION(2,6,25)
 typedef union i387_union FPU_ENV;
 #define TASK_FPENV(tsk)  (&(tsk)->thread.i387)
-#elseif LINUX_VERSION_CODE <= KERNEL_VERSION(2,6,34)
+#elif LINUX_VERSION_CODE <= KERNEL_VERSION(2,6,34)
 typedef union thread_xstate FPU_ENV;
 #define TASK_FPENV(tsk)  ((tsk)->thread.xstate)
 #else

d3b1dd3e55b8db1e938552a19f4984f02511071d
Paolo Mantegazza <paolo.mantegazza@polimi.it>
2011-12-29 15:21:21 +0000

    define init_MUTEX_LOCKED if not defined in Linux

--- base/include/rtai_wrappers.h
+++ base/include/rtai_wrappers.h
@@ -113,6 +113,10 @@ static inline unsigned long hweight_long(unsigned long w)
 
 #else /* LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,0) */
 
+#ifndef init_MUTEX_LOCKED
+#define init_MUTEX_LOCKED(sem)  sema_init(sem, 0)
+#endif
+
 #define RTAI_MODULE_PARM_ARRAY(name, type, addr, size) \
 	module_param_array(name, type, addr, 0400);
 

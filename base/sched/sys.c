ecc1a1a4169a47220a99f4951d71a94e404fd1a9
Paolo Mantegazza <paolo.mantegazza@polimi.it>
2011-12-29 15:40:38 +0000

    export rt_fun_ext for direct use in netrpc.c

--- base/sched/sys.c
+++ base/sched/sys.c
@@ -46,7 +46,8 @@ Nov. 2001, Jan Kiszka (Jan.Kiszka@web.de) fix a tiny bug in __task_init.
 #include <rtai_schedcore.h>
 
 #define MAX_FUN_EXT  16
-static struct rt_fun_entry *rt_fun_ext[MAX_FUN_EXT];
+struct rt_fun_entry *rt_fun_ext[MAX_FUN_EXT];
+EXPORT_SYMBOL(rt_fun_ext);
 
 /* 
  * WATCH OUT for the default max expected size of messages from/to user space.
@@ -609,7 +610,7 @@ static inline long long handle_lxrt_request (unsigned int lxsrq, long *arg, RT_T
 			if ((larg->task)->exectime[0] && (larg->task)->exectime[1]) {
 				larg->exectime[0] = (larg->task)->exectime[0]; 
 				larg->exectime[1] = (larg->task)->exectime[1]; 
-				larg->exectime[2] = rdtsc(); 
+				larg->exectime[2] = rtai_rdtsc(); 
 			}
                         return 0;
 		}

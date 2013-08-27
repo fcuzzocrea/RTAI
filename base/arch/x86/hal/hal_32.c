9405f808969a30cfc1d3cc8f7bf36ccf5bea0ab1
Paolo Mantegazza <paolo.mantegazza@polimi.it>
2013-08-27 19:29:58 +0000

    spaces

--- base/arch/x86/hal/hal_32.c
+++ base/arch/x86/hal/hal_32.c
@@ -7,8 +7,8 @@
  *
  *   Original RTAI/x86 layer implementation: \n
  *   Copyright &copy; 2000-2013 Paolo Mantegazza, \n
- *   Copyright &copy; 2000 Steve Papacharalambous, \n
- *   Copyright &copy; 2000 Stuart Hughes, \n
+ *   Copyright &copy; 2000      Steve Papacharalambous, \n
+ *   Copyright &copy; 2000      Stuart Hughes, \n
  *   and others.
  *
  *   RTAI/x86 rewrite over Adeos: \n

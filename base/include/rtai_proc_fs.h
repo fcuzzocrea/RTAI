72187c5b1453ea2d5a4de39dc897eb5fe25458b0
Paolo Mantegazza <paolo.mantegazza@polimi.it>
2014-02-15 23:32:36 +0000

    preparing for 3.10

--- base/include/rtai_proc_fs.h
+++ base/include/rtai_proc_fs.h
@@ -19,9 +19,53 @@
 #ifndef _RTAI_PROC_FS_H
 #define _RTAI_PROC_FS_H
 
-#define LIMIT (PAGE_SIZE - 80)
-
 extern struct proc_dir_entry *rtai_proc_root;
+
+#if 0
+
+#include <linux/seq_file.h>
+
+#define PROC_READ_FUN(read_fun_name) read_fun_name(struct seq_file *pf, void *v)
+
+#define PROC_READ_OPEN_OPS(rtai_proc_fops, read_fun_name) \
+\
+static int rtai_proc_open(struct inode *inode, struct  file *file) { \
+  return single_open(file, read_fun_name, NULL); \
+} \
+\
+static const struct file_operations rtai_proc_fops = { \
+	.owner = THIS_MODULE, \
+	.open = rtai_proc_open, \
+	.read = seq_read, \
+	.llseek = seq_lseek, \
+	.release = single_release \
+};
+
+static inline void *CREATE_PROC_ENTRY(const char *name, umode_t mode, void *parent, const struct file_operations *proc_fops)
+{
+	return !parent ? proc_mkdir(name, NULL) : proc_create(name, mode, parent, proc_fops);
+}
+
+#define PROC_READ_ENTRY(entry, read_fun)  do { } while(0)
+
+#define PROC_PRINT_VARS 
+
+#define PROC_PRINT_DONE do { return 0; } while(0)
+
+#define PROC_PRINT(fmt, args...)  \
+	do { seq_printf(pf, fmt, ##args); } while(0)
+
+#define PROC_PRINT_RETURN
+
+#else
+
+#define PROC_READ_FUN \
+	static int rtai_read_proc (char *page, char **start, off_t off, int count, int *eof, void *data)
+
+#define PROC_READ_ENTRY(entry, read_fun)  \
+	do { entry->read_proc = read_fun; } while(0)
+
+#define LIMIT (PAGE_SIZE - 80)
 
 // proc print macros - Contributed by: Erwin Rol (erwin@muffin.org)
 
@@ -77,6 +121,7 @@ do {	\
         return len; \
 } while(0)
 
+#endif
 // End of proc print macros
 
 #endif  /* !_RTAI_PROC_FS_H */

86c2b41c10abc96bfcd55d52ce684c65e6741c40
Paolo Mantegazza <paolo.mantegazza@polimi.it>
2015-01-18 15:58:33 +0000

    a few changes to work also without legacy constraints

--- base/include/rtai_schedcore.h
+++ base/include/rtai_schedcore.h
@@ -88,12 +88,12 @@ do { \
 		printk("*** ABORT, NO VIRQ AVAILABLE FOR THE WAKING UP SRQ. ***\n"); \
 		return -1; \
 	} \
-	hal_virtualize_irq(hal_root_domain, wake_up_srq[0].srq, wake_up_srq_handler, NULL, IPIPE_HANDLE_FLAG); \
+	ipipe_request_irq(hal_root_domain, wake_up_srq[0].srq, (void *)wake_up_srq_handler, NULL, NULL); \
 } while (0)
 
 #define RELEASE_RESUME_SRQs_STUFF() \
 do { \
-	hal_virtualize_irq(hal_root_domain, wake_up_srq[0].srq, NULL, NULL, 0); \
+	ipipe_free_irq(hal_root_domain, wake_up_srq[0].srq); \
 	hal_free_irq(wake_up_srq[0].srq); \
 } while (0)
 
@@ -216,8 +216,8 @@ static inline void send_sched_ipi(unsigned long dest)
 #define TASK_NOWAKEUP  TASK_UNINTERRUPTIBLE
 #endif
 
-#define TASK_HARDREALTIME  (TASK_INTERRUPTIBLE | TASK_NOWAKEUP)
-#define TASK_RTAISRVSLEEP  (TASK_INTERRUPTIBLE | TASK_NOWAKEUP)
+#define TASK_HARDREALTIME  (TASK_INTERRUPTIBLE) // | TASK_NOWAKEUP)
+#define TASK_RTAISRVSLEEP  (TASK_INTERRUPTIBLE) // | TASK_NOWAKEUP)
 #define TASK_SOFTREALTIME  TASK_INTERRUPTIBLE
 
 static inline void enq_ready_edf_task(RT_TASK *ready_task)

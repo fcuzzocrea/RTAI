903b12d4e22ea8d3d1c2b10ad5469d66ebf412fe
Paolo Mantegazza <paolo.mantegazza@polimi.it>
2015-01-28 08:04:01 +0000

    bring the random number generator back to arch independent base/include/rtai.h

--- base/include/asm-x86/rtai_hal.h
+++ base/include/asm-x86/rtai_hal.h
@@ -688,37 +688,6 @@ static inline void rt_set_timer_delay(int delay)
 	}
 }
 
-// see: Computing Practices, ACM, vol. 31, n. 10, 1988, pgs 1192-1201.
-
-#define TWOPWR31M1 2147483647  // 2^31 - 1
-
-static inline long next_rand(long rand)
-{
-	const long a = 16807;
-	const long m = TWOPWR31M1;
-	const long q = 127773;
-	const long r = 2836;
-
-	long lo, hi;
-
-	hi = rand/q;
-	lo = rand - hi*q;
-	rand = a*lo - r*hi;
-	if (rand <= 0) {
-		rand += m;
-	}
-	return rand;
-}
-
-static inline long irandu(unsigned long range)
-{
-	static long seed = 783637;
-	const long m = TWOPWR31M1;
-
-	seed = next_rand(seed);
-	return rtai_imuldiv(seed, range, m);
-}
-
 #endif /* __KERNEL__ */
 
 #include <asm/rtai_oldnames.h>

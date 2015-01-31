a083a2d08fb4e67ca38cd2f71b97d3af96a01cee
Paolo Mantegazza <paolo.mantegazza@polimi.it>
2015-01-31 15:34:06 +0000

    deleted a very old kernel support part

--- base/include/asm-x86/rtai_fpu.h
+++ base/include/asm-x86/rtai_fpu.h
@@ -66,23 +66,6 @@
 
 #endif /* CONFIG_RTAI_FPU_SUPPORT */
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,0) && LINUX_VERSION_CODE < KERNEL_VERSION(2,6,11)
-
-#define set_lnxtsk_uses_fpu(lnxtsk) \
-	do { (lnxtsk)->used_math = 1; } while(0)
-#define clear_lnxtsk_uses_fpu(lnxtsk) \
-	do { (lnxtsk)->used_math = 0; } while(0)
-#define lnxtsk_uses_fpu(lnxtsk)  ((lnxtsk)->used_math)
-
-#define set_lnxtsk_using_fpu(lnxtsk) \
-	do { task_thread_info(lnxtsk)->status |= TS_USEDFPU; } while(0)
-//	do { (lnxtsk)->thread_info->status |= TS_USEDFPU; } while(0)
-
-#endif /* LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,0) && LINUX_VERSION_CODE < KERNEL_VERSION(2,6,11) */
-
-
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,11)
-
 #define set_lnxtsk_uses_fpu(lnxtsk) \
 	do { set_stopped_child_used_math(lnxtsk); } while(0)
 #define clear_lnxtsk_uses_fpu(lnxtsk) \
@@ -95,12 +78,11 @@
 #include <asm/fpu-internal.h>
 #define rtai_set_fpu_used(lnxtsk) __thread_set_has_fpu(lnxtsk)
 #else
-#define rtai_set_fpu_used(lnxtsk) do { task_thread_info(lnxtsk)->status |= TS_USEDFPU; } while(0)
+#define rtai_set_fpu_used(lnxtsk) \
+	do { task_thread_info(lnxtsk)->status |= TS_USEDFPU; } while(0)
 #endif
 
 #define set_lnxtsk_using_fpu(lnxtsk) \
 	do { rtai_set_fpu_used(lnxtsk); } while(0)
-
-#endif /* LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,11) */
 
 #endif /* !_RTAI_ASM_X86_FPU_H */

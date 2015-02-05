9b2aa3a9b69b3b3aef068de1856f4ad0af4f61e6
Paolo Mantegazza <paolo.mantegazza@polimi.it>
2015-02-05 16:55:52 +0000

    hided the old declaration of rt_kthread_init

--- base/include/rtai_schedcore.h
+++ base/include/rtai_schedcore.h
@@ -631,7 +631,7 @@ static inline void rtai_cleanup_features (void) {
 
 int rt_check_current_stack(void);
 
-int rt_kthread_init(RT_TASK *task,
+int rt_kthread_init_old(RT_TASK *task,
 		    void (*rt_thread)(long),
 		    long data,
 		    int stack_size,
